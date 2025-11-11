0b25c5640fe2992a247db6d64a0b05257ef75019
Shiful Islam <shwapon.joti@gmail.com>
2025-11-11 13:57:07 +0600

    all added and up to date all code

--- index.js
+++ index.js
@@ -22,23 +22,28 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
+    console.log("Connected to MongoDB!");
 
     const db = client.db("study-db");
     const studyCollection = db.collection("studies");
-    const createPartnerProfileCollection = db.collection("createPartners");
+    // const createPartnerProfileCollection = db.collection("createPartners");
     const findPartnerCollection = db.collection("findPartners");
     const userCollection = db.collection("users");
 
     app.post("/users", async (req, res) => {
       const newUser = req.body;
-      const result = await usersCollection.insertOne(newUser);
+      const result = await userCollection.insertOne(newUser);
       res.send(result);
     })
 
-    //Studies Get API call
+    //Studies Get Top Study Partner API call
     app.get("/studies", async (req, res) => {
       try {
-        const result = await studyCollection.find().toArray();
+        const result = await studyCollection
+        .find()
+        .sort({ rating: -1 })
+        .limit(6)
+        .toArray();
         res.send(result);
       } catch (error) {
         console.error("Database query failed:", error);
@@ -46,13 +51,39 @@ async function run() {
       }
     });
 
-    //Partner Details Get API call
+    //Single Partner Details Get API call
     app.get('/studies/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
       const result = await studyCollection.findOne(query)
       res.send(result);
     })
+
+    //Get Find Partner API call
+    app.get("/find-partners", async (req, res) => {
+      try {
+        console.log("Fetching partners...");
+        const result = await findPartnerCollection
+          .find()
+          .sort({ rating: -1 })
+          .toArray();
+        console.log("Partners found:", result.length);
+        res.send(result);
+      } catch (error) {
+        console.error("Error fetching partners:", error);
+        res.status(500).send({ message: "Failed to fetch partners." });
+      }
+    });
+
+
+    //Find Partner Details Get API call
+    app.get('/find-partners/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await findPartnerCollection.findOne(query)
+      res.send(result);
+    })
+
 
 
     await client.db("admin").command({ ping: 1 });
@@ -63,6 +94,8 @@ async function run() {
     });
   } catch (error) {
     console.error("Critical: Failed to connect to MongoDB.", error);
+  } finally {
+
   }
 }
 
